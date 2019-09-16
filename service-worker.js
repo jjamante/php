fc4ac2e036b755e19a8d27cb39b962ae1fe555c5
jjamante <joeyjamante@gmail.com>
2019-09-16 22:27:01 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,11 @@ const precacheResources = [
   'index.html',
   'logo.png',
   'manifest.json',
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://code.jquery.com/jquery-3.3.1.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
 ];
 
 self.addEventListener('install', event => {
